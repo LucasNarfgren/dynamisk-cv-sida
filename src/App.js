61cfadce186376232a308df722a41074f1793981
LucasNarfgren <narfgren@hotmail.com>
2022-05-16 08:24:13 +0200

    ehm let it work plx again pleaaaase

--- src/App.js
+++ src/App.js
@@ -11,8 +11,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/" element={<App />} />
-        <Route path="/Home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="/Projects" element={<Projects />} />
         <Route path="/Contact" element={<Contact />} />
